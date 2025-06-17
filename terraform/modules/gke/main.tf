2f574d7079141514da6abb90f474164fc2573223
Kyle Schultz <kyle@kjschultz.com>
2025-06-16 21:27:22 -0700

    gke: allow deletion

--- terraform/modules/gke/main.tf
+++ terraform/modules/gke/main.tf
@@ -15,6 +15,7 @@ resource "google_container_cluster" "primary" {
   subnetwork               = var.subnet_id
   remove_default_node_pool = true
   initial_node_count       = var.node_count
+  deletion_protection      = false
 }
 
 resource "google_container_node_pool" "default" {
