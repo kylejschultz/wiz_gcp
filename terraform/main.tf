15b6007571b252f7dbc19e81295cdae3eceeab99
Kyle Schultz <kyle@kjschultz.com>
2025-06-16 20:13:12 -0700

    tf: cleanup main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,18 +10,8 @@ provider "google" {
 }
 
 module "network" {
-  source       = "./modules/network"
+  source = "./modules/network"
   network_name = "wiz-vpc"
   subnet_cidr  = "10.0.0.0/16"
-  region       = "us-west1"
-}
-
-output "vpc_id" {
-  description = "ID of the VPC network from the network module"
-  value       = module.network.vpc_id
-}
-
-output "subnet_id" {
-  description = "ID of the subnetwork from the network module"
-  value       = module.network.subnet_id
+  region = "us-west1"
 }
