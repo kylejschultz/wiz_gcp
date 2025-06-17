16defc6b3ab5db911500e7042efa54acc4b4d3a0
Kyle Schultz <kyle@kjschultz.com>
2025-06-16 20:17:03 -0700

    format main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,17 +1,17 @@
 variable "project_id" {
-    description = "GCP Project ID"
-    type = string
+  description = "GCP Project ID"
+  type        = string
 }
 
 provider "google" {
   project = var.project_id
-  region = "us-west1"
-  zone = "us-west1-a"
+  region  = "us-west1"
+  zone    = "us-west1-a"
 }
 
 module "network" {
-  source = "./modules/network"
+  source       = "./modules/network"
   network_name = "wiz-vpc"
   subnet_cidr  = "10.0.0.0/16"
-  region = "us-west1"
+  region       = "us-west1"
 }
