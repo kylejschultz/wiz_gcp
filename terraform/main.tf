377361314d877273c3a1d102e6b58abe46220f68
Kyle Schultz <kyle@kjschultz.com>
2025-06-21 00:04:53 -0700

    cleanup: remove extra tf stuff

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,3 @@
-terraform {
-  required_version = ">= 1.5.0"
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "~> 5.0"
-    }
-  }
-}
-
 variable "project_id" {
   description = "GCP Project ID"
   type        = string
